65d8891ece49c87d3259b7104afee1cb20bbbd7e
gehenna-yang <winer0707@naver.com>
2024-06-11 13:11:39 +0900

    custom btn add

---
+++ src/constants/colors.ts
@@ -0,0 +1,8 @@
+const colors = {
+  WHITE: '#ffffff',
+  BLACK: '#000000',
+  PINK_500: '#BF5C79',
+  PINK_700: '#C63B64',
+};
+
+export {colors};
--- src/screen/auth/AuthHomeScreen.tsx
+++ src/screen/auth/AuthHomeScreen.tsx
@@ -1,9 +1,10 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import {authNavigation} from '../../constants';
+import CustomButton from '../../components/CustomButton';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -14,12 +15,13 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
     <SafeAreaView>
       <View>
-        <Button
-          title="로그인화면으로 이동"
+        <CustomButton
+          label="로그인화면으로 이동"
           onPress={() => navigation.navigate(authNavigation.LOGIN)}
         />
-        <Button
-          title="회원가입화면으로 이동"
+        <CustomButton
+          label="회원가입화면으로 이동"
+          variant="outlined"
           onPress={() => navigation.navigate(authNavigation.SIGNUP)}
         />
       </View>
