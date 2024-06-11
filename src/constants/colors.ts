2d6b2ee3bf8d869ea02db472e08468f2b435f0fc
gehenna-yang <winer0707@naver.com>
2024-06-11 14:04:34 +0900

    authHome update

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,8 +1,21 @@
 const colors = {
-  WHITE: '#ffffff',
-  BLACK: '#000000',
+  WHITE: '#ffffff', // dark mode black color
+  BLACK: '#161616', // dark mode white color
+  PINK_200: '#FAE2E9',
+  PINK_400: '#EC87A5',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
+  BLUE_500: '#0d8aff',
+  GRAY_100: '#F8F8F8', // dark mode gray 700 color
+  GRAY_200: '#E3E3E3',
+  GRAY_300: '#D8D8D8',
+  GRAY_500: '#8E8E8E', // dark mode gray 500 color
+  GRAY_700: '#575757',
+  DM_GRAY_100: '#202124',
+  DM_GRAY_200: '#3C4043',
+  DM_GRAY_300: '#5E5E5E',
 };
 
 export {colors};
--- src/screen/auth/AuthHomeScreen.tsx
+++ src/screen/auth/AuthHomeScreen.tsx
@@ -1,6 +1,6 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Dimensions, Image, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import {authNavigation} from '../../constants';
@@ -13,8 +13,15 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.continer}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require('../../assets/MATZIP.png')}
+        />
+      </View>
+      <View style={styles.buttonContiner}>
         <CustomButton
           label="로그인화면으로 이동"
           onPress={() => navigation.navigate(authNavigation.LOGIN)}
@@ -29,6 +36,24 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  continer: {
+    flex: 1,
+    margin: 30,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContiner: {
+    flex: 1,
+    gap: 10,
+  },
+});
 
 export default AuthHomeScreen;
