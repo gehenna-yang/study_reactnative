820083b0d5fdaed97d6c5cde0c72f63fadfd44f1
gehenna-yang <winer0707@naver.com>
2024-06-17 00:41:15 +0900

    react-query set & useAuth add

---
+++ src/hooks/queries/useAuth.ts
@@ -0,0 +1,87 @@
+import {useMutation, useQuery} from '@tanstack/react-query';
+import {
+  getAccessToken,
+  getProfile,
+  postLogin,
+  postSignup,
+} from '../../api/auth';
+import {
+  UseMutationCustomOptions,
+  UseQueryCustomOptions,
+} from '../../types/common';
+import {removeEncryptStorage, setEncryptStorage} from '../../utils';
+import {removeHeader, setHeader} from '../../utils/header';
+import {useEffect} from 'react';
+import queryClient from '../../api/queryClient';
+
+function useSignup(mutationOptions?: UseMutationCustomOptions) {
+  return useMutation({
+    mutationFn: postSignup,
+    ...mutationOptions,
+  });
+}
+
+function useLogin(mutationOptions?: UseMutationCustomOptions) {
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: ({accessToken, refreshToken}) => {
+      setEncryptStorage('refreshToken', refreshToken);
+      setHeader('Authorization', `Bearer ${accessToken}`);
+    },
+    onSettled: () => {
+      queryClient.refetchQueries({queryKey: ['auth', 'getAccessToken']});
+      queryClient.invalidateQueries({queryKey: ['auth', 'getProfile']});
+    },
+    ...mutationOptions,
+  });
+}
+
+function useGetRefreshToken() {
+  const {isSuccess, data, isError} = useQuery({
+    queryKey: ['auth', 'getAccessToken'],
+    queryFn: getAccessToken,
+    staleTime: 1000 * 60 * 30 - 1000 * 60 * 3,
+    refetchInterval: 1000 * 60 * 30 - 1000 * 60 * 3,
+    refetchOnReconnect: true,
+    refetchIntervalInBackground: true,
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setHeader('Authorization', `Bearer ${data.accessToken}`);
+      setEncryptStorage('refreshToken', data.refreshToken);
+    }
+  }, [isSuccess, data]);
+
+  useEffect(() => {
+    if (isError) {
+      removeHeader('Authorization');
+      removeEncryptStorage('refreshToken');
+    }
+  }, [isError]);
+
+  return {isSuccess, isError};
+}
+
+function useGetProfile(queryOptions?: UseQueryCustomOptions) {
+  return useQuery({
+    queryKey: ['auth', 'getProfile'],
+    queryFn: getProfile,
+    ...queryOptions,
+  });
+}
+
+function useAuth() {
+  const signupMutation = useSignup();
+  const refreshTokenQuery = useGetRefreshToken();
+  const getProfileQuery = useGetProfile({
+    enabled: refreshTokenQuery.isSuccess,
+  });
+
+  const isLogin = getProfileQuery.isSuccess;
+  const loginMutation = useLogin();
+
+  return {signupMutation, loginMutation, isLogin, getProfileQuery};
+}
+
+export default useAuth;
